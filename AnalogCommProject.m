6a673b0b52b0a3fb7eb3240dc1271a28900bb301
YoussefMohammed <youssif.sayed05@eng-st.cu.edu.eg>
2026-04-10 19:02:13 +0200

    Added the waveforms and a new script to extract them

--- AnalogCommProject.m
+++ AnalogCommProject.m
@@ -5,11 +5,11 @@
 
 clear; clc; close all;
 
-%% 1. Run Preprocessing and Modulator
+%% Run Preprocessing and Modulator
 preprocessing;
 modulator;
 
-%% 2. Shared Filters Setup (IF & LPF)
+%% Shared Filters Setup (IF & LPF)
 F_IF = 15e3;
 
 % IF Filtering Specs
@@ -105,3 +105,5 @@ current_condition = 'Q5 (0.1kHz Offset)';
 receiver;
 
 disp('All processing complete!');
+
+export_figures;
